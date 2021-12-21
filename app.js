083a50cbecc16a2948c7174c76bc49505083173c
George Prince <prince.george35@gmail.com>
2021-12-21 15:36:57 -0600

    Configure session and cookie data

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ var usersRouter = require('./routes/users');
 var config = require('./config.dev');
 var apiUsersRouter = require('./routes/api/users');
 var mongoose = require('mongoose');
-mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 var app = express();
+mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
