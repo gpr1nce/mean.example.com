c048b72d7024d38587026b9e9ceb799a0f8b1665
George Prince <prince.george35@gmail.com>
2021-12-21 15:43:20 -0600

    Serialize the session data

--- app.js
+++ app.js
@@ -46,6 +46,22 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  done(null,{
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name
+  });
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
